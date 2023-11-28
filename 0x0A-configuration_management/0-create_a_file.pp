ebf5d9a2377b2b1d69c013d084f02ff4b5f55ca6
Ade-nike17 <perfectnyke@gmail.com>
2023-11-28 08:14:21 -0800

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This manifest creates a file 'school' at /tmp
 file { '/tmp/school':
     ensure  => 'file',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0744',
     content => 'I love puppet',
 }
